0973d22957e11642c68ecc1c72a1c448e000b3db
aryan <ar4179@columbia.edu>
2024-04-13 15:31:01 -0400

    added homepage

--- server.py
+++ server.py
@@ -3,8 +3,17 @@ from flask import render_template
 from flask import Response, request
 from markupsafe import escape
 
+app = Flask(__name__)
+
 # to protect from injection attacks
 @app.route("/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!"
+
+@app.route('/')
+def home():
+   return render_template('homepage.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
 
